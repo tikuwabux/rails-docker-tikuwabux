e49bee246916b65471d294dc438a1f7f9b472530
tikuwabux <badshinichi@gmail.com>
2022-12-23 12:14:16 +0900

    テストが通るようにテストコードを修正

--- src/test/controllers/users_controller_test.rb
+++ src/test/controllers/users_controller_test.rb
@@ -2,8 +2,6 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "the truth" do
-    # assert true
-    # わざと失敗するように書き換え =>
-    assert false
+    assert true
   end
 end
